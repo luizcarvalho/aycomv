1111f229828b88d2d308ba60e7ab280a307492ce
Luiz Carvalho <maximusmano@gmail.com>
2026-02-25 15:21:53 -0300

    fix config sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: ENV.fetch("SENDGRID_USERNAME"),
-    password: ENV.fetch("SENDGRID_PASSWORD"),
+    user_name: ENV.fetch("SENDGRID_USERNAME", "apikey"),
+    password: ENV.fetch("SENDGRID_PASSWORD", ""),
     domain: "aycom.com.br",
     address: "smtp.sendgrid.net",
     port: 587,
